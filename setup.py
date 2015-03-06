489bfabcdd61c95b52d5ae3acfd5d748cdf6b03c
Bryon Meinka <b.meinka@gmail.com>
2015-03-05 19:45:44 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+readme = open('README.rst').read()
+
+setup(
+    name='venn-places',
+    version='0.0.1',
+    description='Get normalized place data from Foursquare, Yelp, Facebook, Google, and Factual.',
+    long_description=readme,
+    author='Bryon Meinka',
+    author_email='b.meinka@gmail.com',
+    url='http://github.com/vennhq/venn-places-python',
+    packages=['venn'],
+    install_requires=['requests >=2.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python 2.7',
+        'Programming Language :: Python 3.4',
+    ],
+)
